83484411c019024ed58c7e838b009cd5ec6b8e18
HemaMaruthi <Smaruthi.mahi111@gmail.com>
2024-10-21 21:29:19 +0530

    Day 06

--- React/dmart/src/App.js
+++ React/dmart/src/App.js
@@ -3,18 +3,38 @@ import "./App.css";
 // import  FunctionBased  from "./Component/FunctionBased.js";
 
 // import StateManagement from "./Component/Hooks/StateManagement.js";
-// import Groceries from "./Component/Inventory Dhashboard/Groceries.js";
-import ListItem from "./Component/Inventory Dhashboard/ListItem.js";
+// import Groceries from "./Component/InventoryDhashboard/Groceries.js";
+// import ListItem from "./Component/InventoryDhashboard/ListItem.js";
+
+import Electronics from './Component/InventoryDhashboard/Electronics.js'
 
 
 function App() {
-  return (
+
+  return(
     <div>
-      {/* <StateManagement /> */}
-      {/* <Groceries /> */}
-      <ListItem/>
+    <div className='nav-bar'>
+      <div className='logo'>
+        <img src="https://logowik.com/content/uploads/images/dmart-avenue-supermarts4302.jpg"/>
+      </div>
+      <div className="nav-link">Home</div>
+      <div className="nav-link">Home</div>
+      <div className="nav-link">Home</div>
+      <div className="nav-link">Home</div>
+
     </div>
-  );
+    <Electronics/>
+</div>
+  )
+
+
+  // return (
+  //   <div>
+  //     {/* <StateManagement /> */}
+  //     {/* <Groceries /> */}
+  //     <ListItem/>
+  //   </div>
+  // );
   // let showComponent = false;
   // if(showComponent){
   //   console.log("Class")
---
+++ React/dmart/src/Component/InventoryDhashboard/Electronics.js
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import "./Electronics.css";
+
+const Electronics = () => {
+  let [electronic, setEletronic] = useState([
+    {
+      title: "WD 2TB Elements Portable External Hard Drive - USB 3.0 ",
+      price: 64,
+      description:
+        "USB 3.0 and USB 2.0 Compatibility Fast data transfers Improve PC Performance High Capacity; Compatibility Formatted NTFS for Windows 10, Windows 8.1, Windows 7; Reformatting may be required for other operating systems; Compatibility may vary depending on user’s hardware configuration and operating system",
+      category: "electronics",
+      image: "https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_.jpg",
+      rating: {
+        rate: 3.3,
+        count: 203,
+      },
+    },
+  ]);
+  let getElectronics = () => {
+    fetch("https://fakestoreapi.com/products/category/electronics")
+      .then((res) => {
+        // console.log(res.json())
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setEletronic(data);
+      });
+  };
+  return (
+    <div className="container">
+      <div className="electronic-container">
+        {console.log(electronic)}
+
+        {electronic.map((electro) => {
+          console.log(electro);
+
+          return (
+            <div className="device-container">
+              <img src={electro.image} width="200px" height="200px" />
+              <h2>{electro.title}</h2>
+              <div>{electro.description}</div>
+              <h3>Price: {electro.price}</h3>
+              <h3>Rating: {electro.rating.rate}</h3>
+              <h3>{electro.rating.count} reviews</h3>
+            </div>
+          );
+        })}
+        {/* <div>Electronics</div> */}
+        {/* <img src={electronic.image} width="200px" height="200px"/>
+      <h2>{electronic.title}</h2>
+      <div>{electronic.description}</div>
+      <h3>Price: {electronic.price}</h3>
+      <h3>Rating: {electronic.rating.rate}</h3>
+      <h3>{electronic.rating.count} reviews</h3> */}
+
+      </div>
+        <button onClick={getElectronics}>View All</button>
+    </div>
+
+    // <div className="device-container">
+    // {/* <div>Electronics</div> */}
+    // <img src={electronic.image} width="200px" height="200px"/>
+    // <h2>{electronic.title}</h2>
+    // <div>{electronic.description}</div>
+    // <h3>Price: {electronic.price}</h3>
+    // <h3>Rating: {electronic.rating.rate}</h3>
+    // <h3>{electronic.rating.count} reviews</h3>
+    // </div>
+    // <button onClick={getElectronics}>Electronics</button>
+  );
+};
+
+export default Electronics;
