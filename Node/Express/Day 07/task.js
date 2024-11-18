ab34cb2ea2225c21e2793d62eab4341ef52d8f18
HemaMaruthi <Smaruthi.mahi111@gmail.com>
2024-11-18 21:51:33 +0530

    Day 07

---
+++ Node/Express/Day 07/task.js
@@ -0,0 +1,9 @@
+Task 1: Setting Up Dynamic Routes and Route Parameters
+Task:
+Create a route that takes a username as a route parameter and returns a personalized welcome message.
+
+
+
+Task 2: Using Query Strings and Request Body Parsing
+Task:
+Create a route /search that accepts a query string parameter q. If the q parameter is present, return results matching the query; otherwise, return a message saying "No search query provided."
